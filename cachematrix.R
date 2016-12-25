7ddc4fce9673ca3a7b005d1ea220f160f8d5e23f
dimipankratz <dimi.pankratz@googlemail.com>
2016-12-26 00:03:46 +0100

    implemented the makeChacheMatrix and cacheSolve functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Functions that calculate the inverse of a matrix a caches the result.
 
-## Write a short comment describing this function
-
+## Creates a matrix wrapper with a cache for the inverse
 makeCacheMatrix <- function(x = matrix()) {
-
+    i <- NULL
+    set <- function(y) {
+        x <<- y
+        i <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(inverse) i <<- inverse
+    getinverse <- function() i
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
-
+## Calculates the inverse for a given matrix wrapper or reads the result from cache, if it is already available
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    i <- x$getinverse()
+    if(!is.null(i)) {
+        message("getting cached data")
+        return(i)
+    }
+    data <- x$get()
+    i <- solve(data, ...)
+    x$setinverse(i)
+    i
 }
+
+## Example
+## x <- rbind(c(2, 5), c(1, 3))
+## > m <- makeCacheMatrix(x)
+## > cacheSolve(m)
+##      [,1] [,2]
+## [1,]    3   -5
+## [2,]   -1    2
+## > cacheSolve(m) %*% m$get()
+## getting cached data
+##      [,1] [,2]
+## [1,]    1    0
+## [2,]    0    1
